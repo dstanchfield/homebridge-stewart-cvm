a932528ce7e62e47bf6ca57429ef29770fabff9a
Darin Stanchfield <darin@Darins-MacBook-Pro-2.local>
2022-03-31 05:15:28 -0500

    added cvm client

--- src/cvmClient.ts
+++ src/cvmClient.ts
@@ -62,39 +62,39 @@ export class CvmClient {
       ors: '\r\n',
     };
 
-    this.connection.on('data', (data) => {
-      const notification = data.toString().trim();
+    // this.connection.on('data', (data) => {
+    //   const notification = data.toString().trim();
 
-      if (this.rawDataCallback !== null) {
-        this.rawDataCallback.forEach((cb: RawDataCallback) => cb(notification));
-      }
+    //   if (this.rawDataCallback !== null) {
+    //     this.rawDataCallback.forEach((cb: RawDataCallback) => cb(notification));
+    //   }
 
-      switch (notification) {
-        case notifications.POS_16_BY_9_IP:
-        case notifications.POS_16_BY_9_IR:
-          this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_16_BY_9));
-          break;
-        case notifications.POS_1_85_IP:
-        case notifications.POS_1_85_IR:
-          this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_1_85));
-          break;
-        case notifications.POS_2_35_IP:
-        case notifications.POS_2_35_IR:
-          this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_2_35));
-          break;
-        case notifications.POS_4_BY_3_IP:
-        case notifications.POS_4_BY_3_IR:
-          this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_4_BY_3));
-          break;
-        case notifications.POS_USER_1_IP:
-        case notifications.POS_USER_1_IR:
-          this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_USER_1));
-          break;
-        case notifications.POS_USER_2_IP:
-        case notifications.POS_USER_2_IR:
-          this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_USER_2));
-      }
-    });
+    //   switch (notification) {
+    //     case notifications.POS_16_BY_9_IP:
+    //     case notifications.POS_16_BY_9_IR:
+    //       this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_16_BY_9));
+    //       break;
+    //     case notifications.POS_1_85_IP:
+    //     case notifications.POS_1_85_IR:
+    //       this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_1_85));
+    //       break;
+    //     case notifications.POS_2_35_IP:
+    //     case notifications.POS_2_35_IR:
+    //       this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_2_35));
+    //       break;
+    //     case notifications.POS_4_BY_3_IP:
+    //     case notifications.POS_4_BY_3_IR:
+    //       this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_4_BY_3));
+    //       break;
+    //     case notifications.POS_USER_1_IP:
+    //     case notifications.POS_USER_1_IR:
+    //       this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_USER_1));
+    //       break;
+    //     case notifications.POS_USER_2_IP:
+    //     case notifications.POS_USER_2_IR:
+    //       this.changeNotifications.forEach((cb: NotificationCallback) => cb(positions.POS_USER_2));
+    //   }
+    // });
 
     this.connectionPromise = this.connection.connect(params);
   }
